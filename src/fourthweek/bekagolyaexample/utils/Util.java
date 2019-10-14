53953fdc1133e3c7d74110802944a7115afe65ca
progj56 <vzaboji@gmail.com>
2019-10-14 14:03:01 +0200

    4week, 1day (added bekagolyaexample) - Refactoring needed

--- src/fourthweek/bekagolyaexample/utils/Util.java
+++ src/fourthweek/bekagolyaexample/utils/Util.java
@@ -21,10 +21,14 @@ public class Util {
 		for (int i = p.getY()-latotavolsag; i <= p.getY()+latotavolsag; i++) {
 			for (int j = p.getX()-latotavolsag; j <= p.getX()+latotavolsag; j++) {
 				if (mezo.joPozicio(i, j) && !p.equals(new Pozicio(j, i))){
-
+					Cella c= new Cella();
+					c.setAllat(mezo.allatPozicioAlapjan(new Pozicio(j, i)));
+					c.setPozicio(new Pozicio(j, i));
+					eredmeny.add(c);
 				}
 			}
 		}
+		return eredmeny;
 	}
 
 }
